5cd9b52c7857b493a44a94a4f5bc8006f1659cee
woutercallewaert <wouter.callewaert@student.howest.be>
2022-01-25 16:52:06 +0100

    test

--- js/script.js
+++ js/script.js
@@ -3,141 +3,42 @@
 window.addEventListener('load', initialize);
 
 // Elements
-let slcJeopardy;
-let hdgHeader;
-let btnStart, btnValidate;
-let sctOverview, sctZone;
-let currentQuestion;
+let slcTypes;
 
 // Global variables
 let games;
 
 function initialize() {
+
     bindElements();
     addEventListeners();
 
     games = JSON.parse(dataset);
 
-    currentQuestion = 0;
+    fillDropDownList();
 
-    fillDropdown();
 }
 
 function bindElements() {
-    slcJeopardy = document.getElementById("jeopardy-type");
-    hdgHeader = document.getElementById("jeopardy-type-choice");
-    btnStart = document.getElementById("start");
-    sctOverview = document.getElementById("question-overview");
-    sctZone = document.getElementById("question-zone");
+    slcTypes = document.getElementById("jeopardy-type");
 }
 
 function addEventListeners() {
-    slcJeopardy.addEventListener("click", showSelection);
-    btnStart.addEventListener("click", buildQuestions);
+
 }
 
-function fillDropdown() {
-    // for(let rounds in games){
-    //     slcJeopardy.options.add(
-    //         new Option(rounds, rounds)
-    //     );
-
-    //     console.log(games[rounds].round);
-    // //     Undefined for unlogical reasons while in every other
-    // //     JS exercise this works perfectly. Makes 0 sense. This
-    // //     is the main reason why I keep failing at Javascript
-    // //     for 2+ years now.
-    // }
-
-    let rounds = ["Jeopardy!", "Double Jeopardy!"];
-
-
-    rounds.forEach(jeopardyRound => {
-        slcJeopardy.options[slcJeopardy.length] = new Option(jeopardyRound, jeopardyRound);
-    });
-    // games.questions returns Object object
-    // games.questions.round cannot be read (because its not an array)
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
 }
 
-function showSelection() {
-    const selection = slcJeopardy[slcJeopardy.selectedIndex].value;
+function fillDropDownList() {
+    let tests = new Array();
     
-    hdgHeader.innerHTML = `<h1>You're playing ${selection}</h1>`;
-}
+    tests = games.questions.map(a => a.round);
+    var unique = tests.filter(onlyUnique);
 
-function buildQuestions() {
-    let listOfQuestions = new Array();
-    let selectedQuestions = new Array();
-    let questionTable = document.createElement('table');
-    questionTable.id = "tblQuestions";
-    questionTable.innerHTML = "";
-
-    listOfQuestions = games.questions.sort(() => 0.5 - Math.random());
-    selectedQuestions = listOfQuestions.slice(0, 5);
-    console.log(selectedQuestions);
-
-    questionTable.innerHTML = 
-            `
-                <tr>
-                    <td><b>Question</b></td>
-                    <td><b>Answer Given</b></td>
-                    <td><b>Answer</b></td>
-                </tr>
-            `;
-            
-    selectedQuestions.forEach(jeoQuestion => {
-        questionTable.innerHTML += 
-        `
-            <tr>
-                <td>${jeoQuestion.question}</td>
-                <td></td>
-                <td>${jeoQuestion.answer}</td>
-            </tr>
-        `;
-    });
-
-    sctOverview.appendChild(questionTable);
-
-    createQuestion(selectedQuestions);
-}
-
-function createQuestion(selectedQuestions) {
-    let divQuestionZone = document.createElement('div');
-    divQuestionZone.id = "divZone";
-    divQuestionZone.innerHTML = "";
-
-    const button = document.createElement('button');
-    button.textContent = "Next Question";
-    button.id = "validate";
-    button.classList.add("questionbutton");
-
-    for (let index = currentQuestion; index < selectedQuestions.length; index++) {
-        divQuestionZone.innerHTML =
-        `
-        <h2 align="center">Question ${currentQuestion +1}</h2>
-        <h3 align="center">${selectedQuestions[currentQuestion].question}</h3>
-        <div align="center" class="mark">
-            Stake: ${selectedQuestions[currentQuestion].value}
-        </div>
-        <div align="center" class="mark">
-            Wins: 
-        </div>
-        `;
-
-        // <div align="center">
-        //     <button align="center" onclick="validateAnswerAndShowNextQuestion()" id="validate" class="questionbutton">
-        //         Next Question
-        //     </button>
-        // </div>
-
-        divQuestionZone.appendChild(button);
+    for (let i = 0; i < unique.length; i++) {
+        const test = unique[i];
+        slcTypes.options.add(new Option(test, test));
     }
-
-    button.addEventListener("click", validateAnswerAndShowNextQuestion);
-    sctZone.appendChild(divQuestionZone);
-}
-
-function validateAnswerAndShowNextQuestion() {
-    btnValidate = document.getElementById("validate");
-    currentQuestion++;
 }
